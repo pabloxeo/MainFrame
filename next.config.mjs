300481ae200ff03724701062ecd188101a0f7f97
pabloxeoD <pabloxeo@gmail.com>
2024-02-12 00:47:36 +0100

    pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     output: 'export',
-    basePath: '/docs',
+    basePath: '/mainframe',
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
