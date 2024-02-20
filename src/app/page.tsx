5be3a56d0143c0fc76305a2978ad5c3b4c066829
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 19:15:18 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div className="">
-          <img   src="./public/next.svg" alt="ChitChat Logo" />    
+        <div className="shrink-0">
+          <img  className="h-12 w-12" src="./public/next.svg" alt="ChitChat Logo" />    
         </div>
         <div>
         <div className="text-xl font-medium text-black">ChitChat</div>
