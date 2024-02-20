4a63b783642f073434096fa8809d328e56e5426a
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 19:15:01 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent">
+    <main className="">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
         <div className="">
           <img   src="./public/next.svg" alt="ChitChat Logo" />    
