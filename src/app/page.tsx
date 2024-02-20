17f20dbeb0b672c7abb2378fb4170959e82c0495
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 22:26:00 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,9 @@ import "./page.css";
 export default function Home() {
   return (
     <main className="">
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
       <h1 className="text-4xl font-bold">MainFrame</h1>
       <p className="text-lg font-light">
-        Welcome to MainFrame, the future of web development.
+        The portfolio of{"Pablo cantudo"}.<br />
       </p>
     </main>
   );
