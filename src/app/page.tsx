f409126d58885a5e1c1ac2643ee0bc30ce7a65a6
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 18:06:34 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent">
       <h1>Hello World</h1>
+      
     </main>
   );
 }
