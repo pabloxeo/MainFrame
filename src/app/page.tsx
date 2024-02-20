ed5970cd567428cc7b94f8b8fc601e77612474a0
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 18:38:37 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <main className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
         <div className="shrink-0">
-          <img  className="h-12 w-12" src="/public/next.svg" alt="ChitChat Logo" />    
+          <img  className="h-12 w-12" src="./public/next.svg" alt="ChitChat Logo" />    
         </div>
         <div>
         <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-slate-500">You have a new message!</p>
+          <p className="text-slate-500">You have a new mesage!</p>
         </div>
       </div>
     </main>
