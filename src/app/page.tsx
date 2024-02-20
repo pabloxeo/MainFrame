c5ac18701bad42f77b0e5ed0d7416d2e7143e70f
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 18:35:39 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Pablo Cantudo</h1>
-      
+    <main className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+          <Image
+            className="h-12 w-12"
+            src="/img/logo.svg"
+            alt="ChitChat Logo"
+            width={48}
+            height={48}
+          />
+        </div>
+        <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
     </main>
   );
 }
