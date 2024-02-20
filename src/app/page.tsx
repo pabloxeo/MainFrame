2ef8c03bad4ae720bdccf72cd968c6be4bf286a4
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 19:20:20 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div className="p-6 bg-white rounded-xl shadow-lg flex items-center space-x-4">
         
       </div>
     </main>
