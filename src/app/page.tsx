23db76126554edcd5a769d8cbfe5b7244bbf0e97
pabloxeoD <pabloxeo@gmail.com>
2024-02-20 18:26:23 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent">
+    <main className="">
       <h1>Pablo Cantudo</h1>
       
     </main>
