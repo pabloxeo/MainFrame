08a1e54d51d6e719fa90ff63eb35fdd699637ce4
pabloxeoD <pabloxeo@gmail.com>
2024-02-21 18:13:49 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,22 @@ import "./page.css";
 
 import React from 'react';
 
-/*export default function Home() {
-  return (
-    <main className="">
-      <h1 className="text-4xl font-bold">MainFrame</h1>
-      <p className="text-lg font-light">
-        The portfolio of Pablo cantudo.<br />
-      </p>
-    </main>
-  );
-*/
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">MainFrame</h1>
       <p className="text-lg font-light">
         The portfolio of Pablo cantudo.<br />
       </p>
-      <ul data-flip="">
+      <ul className="grid grid-cols-36 gap-1">
         {(new Array(810)).fill(null).map((_, i) => (
-          <li key={i}>
-            <div className="flipper">
-              <div className="flipper__dot">
-                <span className="flipper__front">
-                  {/* Front content */}
-                </span>
-                <span className="flipper__back">
-                  {/* Back content */}
-                </span>
+          <li key={i} className="w-6 h-6">
+            <div className="group relative w-full h-full">
+              <div className="absolute inset-0 bg-black rounded-md opacity-0 group-hover:opacity-100 transform transition-transform">
+                {/* Front content */}
+              </div>
+              <div className="absolute inset-0 bg-white rounded-md opacity-100 group-hover:opacity-0 transform transition-transform rotate-y-180">
+                {/* Back content */}
               </div>
             </div>
           </li>
