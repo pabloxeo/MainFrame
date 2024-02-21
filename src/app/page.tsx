ec228dfb2e7f68e2d8611b87568b3db1605393f0
pabloxeoD <pabloxeo@gmail.com>
2024-02-21 18:05:02 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 import Image from "next/image";
 import "./page.css";
 
+import React from 'react';
+
+const FlipDotList: React.FC = () => {
+  return (
+    <ul data-flip="">
+      {Array.from({ length: 810 }, (_, i) => (
+        <li key={i}>
+          <div className="flipper">
+            <div className="flipper__dot">
+              <span className="flipper__front"> {/* front content */}</span>
+              <span className="flipper__back"> {/* back content */}</span>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export { FlipDotList };
+
 export default function Home() {
   return (
     <main className="">
