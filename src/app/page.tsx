ea83f1eb72f95dfd82e92675d34daf247a276493
pabloxeoD <pabloxeo@gmail.com>
2024-02-21 18:08:30 +0100

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,37 @@ import "./page.css";
 
 import React from 'react';
 
-const FlipDotList: React.FC = () => {
-  return (
-    <ul data-flip="">
-      {Array.from({ length: 810 }, (_, i) => (
-        <li key={i}>
-          <div className="flipper">
-            <div className="flipper__dot">
-              <span className="flipper__front"> {/* front content */}</span>
-              <span className="flipper__back"> {/* back content */}</span>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-export { FlipDotList };
-
-export default function Home() {
+/*export default function Home() {
   return (
     <main className="">
       <h1 className="text-4xl font-bold">MainFrame</h1>
       <p className="text-lg font-light">
         The portfolio of Pablo cantudo.<br />
       </p>
+    </main>
+  );
+*/
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">MainFrame</h1>
+      <p className="text-lg font-light">
+        The portfolio of Pablo cantudo.<br />
+      </p>
+      <ul className="grid grid-cols-36 gap-1">
+        {(new Array(810)).fill(null).map((_, i) => (
+          <li key={i} className="w-6 h-6">
+            <div className="group relative w-full h-full">
+              <div className="absolute inset-0 bg-black rounded-md opacity-0 group-hover:opacity-100 transform transition-transform">
+                {/* Front content */}
+              </div>
+              <div className="absolute inset-0 bg-white rounded-md opacity-100 group-hover:opacity-0 transform transition-transform rotate-y-180">
+                {/* Back content */}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
